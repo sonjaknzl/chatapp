949530873c20af6cdb378d22ad6d8eed0a5378f7
sonjaknzl <sonjakuenzl@aol.com>
2021-10-27 11:11:49 +0200

    first draft

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.getElementById("login").addEventListener("click", login);
+
+function login() {
+    let username = document.getElementById("username").value;
+    console.log(username);
+}
