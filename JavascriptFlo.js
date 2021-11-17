b10a22b88f4a6af474e7e5b4c7b98aaf56e51422
flo2106 <muellerflorian2002@gmail.com>
2021-11-17 17:30:34 +0100

    JS Progress

---
+++ JavascriptFlo.js
@@ -0,0 +1,10 @@
+var searchbar = document.getElementById("search");
+var searchreq = document.getElementById("searchbutton");
+
+searchreq.addEventListener("click", request)
+
+function request(){
+    searchbar.classList.add("open");
+    var currenttext = searchbar.ariaValueText;
+}
+
