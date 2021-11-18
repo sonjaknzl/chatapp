69225a7d23aba0e121762c9569f3c949d80c9363
Soulshaker69 <62985030+Soulshaker69@users.noreply.github.com>
2021-11-18 16:22:20 +0100

    senden läuft

--- andresJS.js
+++ andresJS.js
@@ -1,96 +1,89 @@
 
 
 //User Informationen
-let chatUser = "/Jerry";
 window.chatToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM3MTk5MDMxfQ.cBHfStPmORIZXQMenda4FIODPY79wvqCvSzmeEtzf9s";
 window.chatCollectionId = "e50a4ae0-c42b-4fb5-93f3-24a961b1cca8";
 window.chatServer = "https://online-lectures-cs.thi.de/chat";
 
+var chatUser = "/Jerry";
+getMessages();
 
+function getMessages(){
+    
+    var chatField = document.getElementById("chatField");
+    
+    while (chatField.hasChildNodes()){
+        chatField.removeChild(chatField.firstChild);
+    }
+    
+    var xmlhttp = new XMLHttpRequest();
 
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) { 
-        let data = JSON.parse(xmlhttp.responseText);
+    // Chat Server URL und Collection ID als Teil der URL
+    xmlhttp.open("GET", window.chatServer + "/" + window.chatCollectionId +"/message"+ chatUser, true);
 
-        for(let element of data){
-            
-            let pName = document.createElement('p');
-            pName.textContent = element.from+": ";
-            chatField.appendChild(pName);
-
-            let sMessage = document.createElement('span');
-            sMessage.textContent = element.msg;
-            pName.appendChild(sMessage);
-
-            let sTime = document.createElement('span');
-
-            var timestamp = element.time;
-            var date = new Date(timestamp);
-            sTime.textContent = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-            sTime.className = "time";
-            pName.appendChild(sTime);
-
-
- 
-
-            /*
-            const chatFrom = document.getElementById('name');
-            let from = element.from+":";
-            chatFrom.innerText = from;
-
-            const chatMsg = document.getElementById('msg'); 
-            let msg = element.msg;
-            chatMsg.innerText = msg;
-
-            const chatTime = document.getElementById('time'); 
-            let time = "Datum:"+element.time;
-            chatTime.innerText = time;
-            */
-        }
-
-        console.log(data);
-        
-    } 
-};
-
-
-// Chat Server URL und Collection ID als Teil der URL
-xmlhttp.open("GET", window.chatServer + "/" + window.chatCollectionId +"/message"+ chatUser, true);
-// Das Token zur Authentifizierung, wenn notwendig 
-xmlhttp.setRequestHeader('Authorization', 'Bearer ' + window.chatToken); 
-xmlhttp.send();
-
-window.setInterval(function() { console.log("Hallo, Welt!");
-}, 1000);
-
-
-
-
-
-
-document.getElementById("msgSend").onclick = function() {myFunction()};
-
-function myFunction() {
-    let xmlhttp = new XMLHttpRequest();
+    // Das Token zur Authentifizierung, wenn notwendig 
+    xmlhttp.setRequestHeader('Authorization', 'Bearer ' + window.chatToken); 
+    
+    
     xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 204) {
-            console.log("done...");
-        }
-    };
-    xmlhttp.open("POST", "https://online-lectures-cs.thi.de/chat/e50a4ae0-c42b-4fb5-93f3-24a961b1cca8/message", true);
-    xmlhttp.setRequestHeader('Content-type', 'application/json');
-    // Add token, e. g., from Tom
-    xmlhttp.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM3MTk5MDMxfQ.cBHfStPmORIZXQMenda4FIODPY79wvqCvSzmeEtzf9s');
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) { 
+            let data = JSON.parse(xmlhttp.responseText);
     
-    // Create request data with message and receiver
-    let msg = document.getElementById("msgNew").value;
+            for(let element of data){
+                
+                let pName = document.createElement('p');
+                pName.textContent = element.from+": ";
+                chatField.appendChild(pName);
     
-    let data = {
-        message: msg,
-        to: "Tom"
+                let sMessage = document.createElement('span');
+                sMessage.textContent = element.msg;
+                pName.appendChild(sMessage);
+    
+                let sTime = document.createElement('span');
+    
+                var timestamp = element.time;
+                var date = new Date(timestamp);
+                sTime.textContent = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+                sTime.className = "time";
+                pName.appendChild(sTime);
+    
+            }            
+        } 
     };
-    let jsonString = JSON.stringify(data); // Serialize as JSON
-    xmlhttp.send(jsonString); // Send JSON-data to server
+
+    xmlhttp.send();
+    console.log("hi1");
 }
 
+
+var sendButton = document.getElementById("msgSend");
+sendButton.addEventListener("click", sendMessage);
+
+function sendMessage(){
+
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST", window.chatServer+"/"+ window.chatCollectionId+"/message", true);
+
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.readyState == 44 && xmlhttp.status == 204){
+            console.log("XML HTTP ACTIVE");
+        }
+    };
+
+    // xmlhttp.open("POST", window.chatServer+"/"+ window.chatCollectionId+"/message", true);
+
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+
+    xmlhttp.setRequestHeader("Authorization", "Bearer window.chatToken");
+
+    // Create request data with message and receiver
+    var newMessage = document.getElementById("msgNew").value;
+    let data = {
+    message: newMessage,
+    to: "Jerry",
+    };
+
+    let jsonString = JSON.stringify(data); // Serialize as JSON
+    xmlhttp.send(jsonString); // Send JSON-data to server
+    window.setTimeout(getMessages(), 2000);
+}
