754653a52d31b7d88799c1808ea4d1e92c6bd850
sonjaknzl <sonjakuenzl@aol.com>
2021-12-13 00:42:01 +0100

    ok

--- Model/User.php
+++ Model/User.php
@@ -1,14 +1,25 @@
 <?php
 namespace Model;
-
-class User{
+use JsonSerializable;
+class User implements JsonSerializable {
     private $username;
 
     public function __construct($username){
         $this->username = $username;
     }
-    
+
     public function getUsername(){
         return $this->username;
+    }
+
+    public function jsonSerialize() { 
+        return get_object_vars($this);
+    }
+    public static function fromJson($data){
+        $user = new User("");
+        foreach ($data as $key => $value) { 
+            $user->{$key} = $value;
+
+        }
     }
 }
--- chat.php
+++ chat.php
@@ -1,5 +1,11 @@
 <?php 
 require("start.php");
+$user = new Model\User("Test");
+$json = json_encode($user);
+echo $json . "<br>";
+$jsonObject = json_decode($json);
+$newUser = Model\User::fromJson($jsonObject); 
+var_dump($newUser);
 ?>
 <!DOCTYPE html>
 <html lang="en">
